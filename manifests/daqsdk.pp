4b1c025e6af0692957d65dc8c54ced8a19efc7cc
Joshua Hoblitt <josh@hoblitt.com>
2022-06-10 09:01:26 -0700

    add daq::daqsdk class

---
+++ manifests/daqsdk.pp
@@ -0,0 +1,71 @@
+#
+# @summary Installs LSST DAQ SDK software
+#
+# @param repo_url
+#   DAQ SDK Nexus repo
+#
+# @param version
+#   DAQ SDK relase version string
+#
+class daq::daqsdk (
+  Stdlib::HTTPUrl $repo_url = 'https://repo-nexus.lsst.org/nexus/repository/daq/daq-sdk',
+  String          $version  = 'R5-V3.2'
+) {
+  require daq
+
+  $daq_base_path = "${daq::base_path}/daq-sdk"
+  $dl_path       = "${daq_base_path}/dl"
+  $archive_name  = "${version}.tgz"
+  $dl_file       = "${dl_path}/${archive_name}"
+  $source        = "${repo_url}/${archive_name}"
+  $install_path  = "${daq_base_path}/${version}"
+
+  file { $daq_base_path:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    #recurse => true,
+    #purge   => true,
+    #force   => true,
+  }
+
+  file { $dl_path:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  archive { $dl_file:
+    ensure       => present,
+    cleanup      => false,
+    creates      => $install_path,
+    extract_path => $daq_base_path,
+    extract      => true,
+    source       => $source,
+    require      => File[$daq_base_path],
+  }
+  ~> file { $dl_file:
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  # chown untar dir
+  file { $install_path:
+    owner     => 'root',
+    group     => 'root',
+    recurse   => true,
+    max_files => 10000,
+    require   => Archive[$dl_file],
+  }
+
+  file { "${daq_base_path}/current":
+    ensure => link,
+    owner  => 'root',
+    group  => 'root',
+    target => $install_path,
+  }
+}
---
+++ spec/acceptance/daqsdk_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'daq::daqsdk class' do
+  let(:pp) do
+    <<-PP
+    accounts::user { 'dsid': }
+    accounts::user { 'rce': }
+
+    include daq::daqsdk
+    PP
+  end
+
+  it_behaves_like 'an idempotent resource'
+
+  %w[
+    /opt/lsst/daq-sdk
+    /opt/lsst/daq-sdk/dl
+    /opt/lsst/daq-sdk/R5-V3.2
+    /opt/lsst/daq-sdk/R5-V3.2/alinux
+    /opt/lsst/daq-sdk/R5-V3.2/examples
+    /opt/lsst/daq-sdk/R5-V3.2/include
+    /opt/lsst/daq-sdk/R5-V3.2/rtems
+    /opt/lsst/daq-sdk/R5-V3.2/x86
+  ].each do |dir|
+    describe file(dir) do
+      it { is_expected.to be_directory }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_mode '755' } # serverspec does not like a leading 0
+    end
+  end
+
+  describe file('/opt/lsst/daq-sdk/current') do
+    it { is_expected.to be_symlink }
+    it { is_expected.to be_linked_to '/opt/lsst/daq-sdk/R5-V3.2' }
+    it { is_expected.to be_owned_by 'root' }
+    it { is_expected.to be_grouped_into 'root' }
+  end
+
+  describe file('/opt/lsst/daq-sdk/dl/R5-V3.2.tgz') do
+    it { is_expected.to be_file }
+    it { is_expected.to be_owned_by 'root' }
+    it { is_expected.to be_grouped_into 'root' }
+    it { is_expected.to be_mode '644' } # serverspec does not like a leading 0
+  end
+end

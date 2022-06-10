3c14480d8c61d1f6ad8a2d57e6e850ca909a410d
Joshua Hoblitt <josh@hoblitt.com>
2022-06-10 15:10:44 -0700

    add a dummy lsst-daq interface for acceptance tests
    The rce service appears to have a hardwired check for the existence of
    an interface named lsst-daq and will fail to startup if it is not
    present.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,6 +4,18 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker
 
+# The rce service appears to have a hardwired check for the existence of an
+# interface named lsst-daq and will fail to startup if it is not present.
+script = <<~SH
+  ip link add name lsst-daq link eth0 type dummy
+  ip a add 192.168.42.1/24 dev lsst-daq
+  ip link set lsst-daq up
+SH
+
+script.split("\n").each do |line|
+  shell(line)
+end
+
 shared_examples 'an idempotent resource' do
   it 'applies with no errors' do
     apply_manifest(pp, catch_failures: true)

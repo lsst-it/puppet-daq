6445d953d1832fb1da03a30f3e3da5d13290699d
Joshua Hoblitt <josh@hoblitt.com>
2022-07-15 10:32:31 -0700

    create libreadline.so.6 symlink on EL8
    EL8 provides libreadline.so.7 and there does not appear to be a
    compatibility package for libreadline.so.6.  The dsid and rce services
    both fail to startup as they are linked against libreadline.so.6.
    
    E.g.:
    
        Jul 15 17:06:49 centos8-64.example.com dsid_startup[3479]: /opt/lsst/daq-sdk/current/x86/bin/dsid_startup: error while loading shared libraries: libreadline.so.6: cannot open shared object file: No such file or directory

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -17,4 +17,13 @@ describe 'daq class' do
     it { is_expected.to be_grouped_into 'root' }
     it { is_expected.to be_mode '755' } # serverspec does not like a leading 0
   end
+
+  if fact('os.release.major') == '8'
+    describe file('/usr/lib64/libreadline.so.6') do
+      it { is_expected.to be_symlink }
+      it { is_expected.to be_linked_to 'libreadline.so.7.0' }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+    end
+  end
 end
